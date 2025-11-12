2a1635d3b5f0beaa09977383b9ab719d1cccb9b0
JeonDongJun <jdj9821@naver.com>
2025-11-12 09:18:31 +0900

    edit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-EPOCHS_DENSE=50
-EPOCHS_PRUNE=10
-BATCH_SIZE=256
+EPOCHS_DENSE=20
+EPOCHS_PRUNE=1
+BATCH_SIZE=512
 SPARSITIES=(0.3 0.5 0.7 0.9)
 
 python main.py --generate_plots --sparsities ${SPARSITIES[@]} --method all --epochs_dense $EPOCHS_DENSE --epochs_prune $EPOCHS_PRUNE --batch_size $BATCH_SIZE
